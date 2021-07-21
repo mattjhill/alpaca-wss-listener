81fc5d57f422d1c57df2fdc2cfcd172d252ff9c9
Matthew Hill <mhill92@gmail.com>
2021-07-20 21:05:18 -0400

    add alpaca listener

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import logging
+
+from alpaca_trade_api.stream import Stream
+
+log = logging.getLogger(__name__)
+
+
+async def handle_event(event):
+    print(event)
+
+def main():
+    stream = Stream(data_feed='sip', raw_data=True)
+    stream.subscribe_trade_updates(handle_event)
+    stream.subscribe_bars(handle_event)
+    stream.run()
+
 if __name__ == "__main__":
-    print("hello, heroku")
+    main()
