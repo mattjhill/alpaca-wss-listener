1d4ddef6862705c803d17ec9b65221e730ec55cb
Matthew Hill <mhill92@gmail.com>
2021-07-20 21:11:26 -0400

    listen to all symbol bars

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     logging.basicConfig(level=logging.INFO)
     stream = Stream(data_feed='sip', raw_data=True)
     stream.subscribe_trade_updates(handle_event)
-    stream.subscribe_bars(handle_event)
+    stream.subscribe_bars(handle_event, '*')
     stream.run()
 
 if __name__ == "__main__":
