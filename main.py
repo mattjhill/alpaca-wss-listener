28e69611527ba4819f0176e448e8d1a58748c629
Matthew Hill <mhill92@gmail.com>
2021-07-20 21:25:12 -0400

    use paper account url

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from alpaca_trade_api.stream import Stream
+from alpaca_trade_api.common import URL
 
 log = logging.getLogger(__name__)
 
@@ -8,9 +9,19 @@ log = logging.getLogger(__name__)
 async def handle_event(event):
     log.info(event)
 
+def run_connection(conn):
+    try:
+        conn.run()
+    except Exception as e:
+        print(f'Exception from websocket connection: {e}')
+    finally:
+        print("Trying to re-establish connection")
+        time.sleep(3)
+        run_connection(conn)
+
 def main():
     logging.basicConfig(level=logging.INFO)
-    stream = Stream(data_feed='sip', raw_data=True)
+    stream = Stream(data_feed='sip', raw_data=True, base_url=URL('https://paper-api.alpaca.markets'))
     stream.subscribe_trade_updates(handle_event)
     stream.subscribe_bars(handle_event, '*')
     stream.run()
